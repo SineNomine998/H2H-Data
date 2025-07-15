4444e75c3fd1a5a1880bbce12af49b0eb66871d7
Enes Yavuz Kozan <eykozan2004@gmail.com>
2025-07-15 17:45:56 +0200

    Add service, repo, PatientRecord and mapper classes

---
+++ patient-service/src/main/java/com/sinenomine/patientservice/PatientRepository.java
@@ -0,0 +1,8 @@
+package com.sinenomine.patientservice;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PatientRepository extends JpaRepository<PatientModel, Long> {
+}
