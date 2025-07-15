3179a72361e2ca0e7f8b6ff24fea9946ab055ece
Enes Yavuz Kozan <eykozan2004@gmail.com>
2025-07-15 22:20:19 +0200

    Create a draft patient.proto and gRPC service

---
+++ patient-service/src/main/java/com/sinenomine/patientservice/GrpcPatientService.java
@@ -0,0 +1,8 @@
+package com.sinenomine.patientservice;
+
+import net.devh.boot.grpc.server.service.GrpcService;
+
+@GrpcService
+public class GrpcPatientService { // TODO: make it extend the generated grpc base class
+    
+}
